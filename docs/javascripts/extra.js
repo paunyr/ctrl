b493fede2f43dd43c597ee1c048ff2fa68591dd3
fanny <fanny@pen>
2025-05-08 13:31:37 +0200

    Wrong path

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,4 +1,4 @@
 let burger_label = document.querySelector(".md-icon");
 
-burger_label.innerHTML = "<img id='mini-logo' src='./../resources/logos/ctrl_logo_pink.png'>"
+burger_label.innerHTML = "<img id='mini-logo' src='../resources/logos/ctrl_logo_pink.png'>"
 
